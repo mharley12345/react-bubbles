c4b7016febcc13127b530e983ad78fd4a0e297fb
Mike Harley <mharley12345@gmail.com>
2019-09-20 08:28:43 -0700

    answers ?'s sets up post request, login form, gets 200 with token from api

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,14 +1,71 @@
-import React from "react";
+import React from 'react'
+import {withFormik, Form, Field} from 'formik'
+import * as Yup from 'yup';
+import {axiosWithAuth} from "../utils/axiosWithAuth"
 
-const Login = () => {
-  // make a post request to retrieve a token from the api
-  // when you have handled the token, navigate to the BubblePage route
-  return (
-    <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
-  );
-};
 
-export default Login;
+ const LoginForm = ({ touched, errors, isSubmitting, values }) => {
+    return (
+        <Form>
+        <div className="input-group">
+            <div className="login-group" >
+               {touched.name && errors.name && <p>{errors.username}</p>}
+               <label htmlFor="username">Username: </label>
+                <Field
+                    type="text"
+                    name="username"
+                    placeholder="Username"
+
+                />
+            </div>
+            <div className="login-group">
+            {touched.password && errors.password && <p>{errors.password}</p>}
+                <label htmlFor="password">Password: </label>
+                <Field
+                    type="password"
+                    name="password"
+                    placeholder="Password"
+
+                />
+            </div>
+     
+        </div>
+        <div>
+                {isSubmitting && <p>Loading...</p>}
+            <button
+                disabled={isSubmitting}
+                className="submit-button"
+                type="submit"
+            >
+                Submit
+            </button>
+        </div>
+        </Form>
+    )
+}
+
+const FormikLoginForm = withFormik({
+    mapPropsToValues() {
+       return{
+        username: "",
+        password: ""
+       };
+    },
+
+    validationSchema: Yup.object().shape({
+        username: Yup.string().required(),
+        password: Yup.string().min( 3, 'Password must be at least 3 characters').required('Password Required')
+    }),
+
+    handleSubmit(values,formikBag){
+        const url = "/login"
+        return axiosWithAuth().post(url,values).then(res => {
+            localStorage.setItem("token", res.data.payload);
+            formikBag.props.history.push("/protected")
+            formikBag.resetForm()
+        })
+
+    },
+
+})(LoginForm);
+export default FormikLoginForm;
