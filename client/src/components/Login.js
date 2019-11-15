ac7583f93bda4fa96c917be172cdcfb1ce93880e
Mike Harley <mharley12345@gmail.com>
2019-11-14 17:55:15 -0800

    Update Login.js

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -7,8 +7,8 @@ const Login = props => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
   const [login, setLogin] = useState({
-    username: "",
-    password: ""
+    username: "mike",
+    password: "mike12"
   });
 
   const handleChange = e => {
